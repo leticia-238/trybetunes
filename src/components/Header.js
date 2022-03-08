421f847890b4847a128c8e3dad2b1e43f9efad83
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-03-08 19:35:08 -0300

    Cria os links de navagaçao no cabeçalho - Requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import LoadingMessage from './LoadingMessage';
 import LogoHeader from '../assets/logoHeader.png';
@@ -23,23 +24,45 @@ class Header extends React.Component {
       <div>
         {user
           ? (
-            <header data-testid="header-component" className="header">
-              <img
-                src={ LogoHeader }
-                alt="Logo TrybeTunes"
-                className="logo-header"
-              />
-              <div className="user-box-header">
-                <div className="header-user-image" />
-                <p
-                  data-testid="header-user-name"
-                  className="header-user-name"
-                >
-                  { user.name }
+            <>
+              <header data-testid="header-component" className="header">
+                <img
+                  src={ LogoHeader }
+                  alt="Logo TrybeTunes"
+                  className="logo-header"
+                />
+                <div className="user-box-header">
+                  <div className="header-user-image" />
+                  <p
+                    data-testid="header-user-name"
+                    className="header-user-name"
+                  >
+                    { user.name }
 
-                </p>
-              </div>
-            </header>
+                  </p>
+                </div>
+              </header>
+              <nav>
+                <Link
+                  to="/search"
+                  data-testid="link-to-search"
+                >
+                  <div>Pesquisa</div>
+                </Link>
+                <Link
+                  to="/favorites"
+                  data-testid="link-to-favorites"
+                >
+                  <div>Favoritas</div>
+                </Link>
+                <Link
+                  to="/profile"
+                  data-testid="link-to-profile"
+                >
+                  <div>Perfil</div>
+                </Link>
+              </nav>
+            </>
           )
           : <LoadingMessage />}
       </div>
