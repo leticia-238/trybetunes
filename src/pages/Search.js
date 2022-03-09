16fbb45f57ca04062e715ccf6a8f7c1b384100ad
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-03-08 21:35:37 -0300

    Cria o formulario para pesquisar artistas - Requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,47 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      artistName: '',
+    };
+  }
+
+  handleInput = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+  }
+
+  searchArtist = () => {
+    //
+  }
+
   render() {
+    const { artistName } = this.state;
+    const minNumOfChars = 2;
+
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <label htmlFor="search-artist-input">
+          Nome do Artista
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            id="search-artist-input"
+            name="artistName"
+            value={ artistName }
+            onChange={ this.handleInput }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          onClick={ this.searchArtist }
+          disabled={ artistName.length < minNumOfChars }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
