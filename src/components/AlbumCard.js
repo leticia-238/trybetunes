b49461ac56adcefed6ff01c5452ce526c1da0d04
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-03-09 12:23:51 -0300

    Faz requisiçao para pesquisar artistas - Requisito 6

---
+++ src/components/AlbumCard.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+class AlbumCard extends React.Component {
+  render() {
+    const { artistName, collectionName, artworkUrl100, collectionId } = this.props;
+    return (
+      <div>
+        <Link
+          to={ `/album/${collectionId}` }
+          data-testid={ `link-to-album-${collectionId}` }
+        >
+          <img src={ artworkUrl100 } alt={ collectionName } />
+          <h3>{collectionName}</h3>
+          <p>{artistName}</p>
+        </Link>
+      </div>
+    );
+  }
+}
+
+AlbumCard.propTypes = {
+  artistName: PropTypes.string.isRequired,
+  collectionName: PropTypes.string.isRequired,
+  artworkUrl100: PropTypes.string.isRequired,
+  collectionId: PropTypes.number.isRequired,
+};
+
+export default AlbumCard;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import LoadingMessage from '../components/LoadingMessage';
+import AlbumCard from '../components/AlbumCard';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
       artistName: '',
+      searchArtistIsLoading: false,
+      searchArtistIsFinished: false,
+      albumData: [],
+      saveArtistName: '',
     };
   }
 
@@ -13,38 +20,69 @@ class Search extends React.Component {
     this.setState({ [name]: value });
   }
 
-  searchArtist = () => {
-    //
+  searchArtist = async (artist) => {
+    this.setState(
+      { artistName: '', searchArtistIsLoading: true, saveArtistName: artist },
+    );
+    const response = await searchAlbumsAPI(artist);
+    this.setState({
+      searchArtistIsLoading: false,
+      searchArtistIsFinished: true,
+      albumData: response,
+    });
   }
 
   render() {
-    const { artistName } = this.state;
+    const { artistName, searchArtistIsLoading, searchArtistIsFinished,
+      albumData, saveArtistName } = this.state;
     const minNumOfChars = 2;
 
     return (
       <div data-testid="page-search">
         <Header />
-        <form className="search-box">
-          <input
-            type="text"
-            data-testid="search-artist-input"
-            id="search-artist-input"
-            name="artistName"
-            value={ artistName }
-            onChange={ this.handleInput }
-            placeholder="Nome do Artista"
-            className="input-field"
-          />
-          <button
-            type="button"
-            data-testid="search-artist-button"
-            onClick={ this.searchArtist }
-            disabled={ artistName.length < minNumOfChars }
-            className="btn"
-          >
-            Pesquisar
-          </button>
-        </form>
+        { searchArtistIsLoading
+          ? <LoadingMessage />
+          : (
+            <>
+              <form className="search-box">
+                <input
+                  type="text"
+                  data-testid="search-artist-input"
+                  id="search-artist-input"
+                  name="artistName"
+                  value={ artistName }
+                  onChange={ this.handleInput }
+                  placeholder="Nome do Artista"
+                  className="input-field"
+                />
+                <button
+                  type="button"
+                  data-testid="search-artist-button"
+                  onClick={ () => this.searchArtist(artistName) }
+                  disabled={ artistName.length < minNumOfChars }
+                  className="btn"
+                >
+                  Pesquisar
+                </button>
+              </form>
+              { searchArtistIsFinished
+                ? (
+                  <>
+                    <p>
+                      { `Resultado de álbuns de: ${saveArtistName}` }
+                    </p>
+                    <div>
+                      { albumData.length > 0
+                        ? albumData.map((album) => (
+                          <AlbumCard { ...album } key={ album.collectionId } />
+                        ))
+                        : <span>Nenhum álbum foi encontrado</span>}
+                    </div>
+                  </>
+                )
+                : ''}
+            </>
+          )}
       </div>
     );
   }
