2f238c018302fdc27c86f8fb6890b00fd934db79
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-03-17 19:16:12 -0300

    Requisito 13

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import LoadingMessage from '../components/LoadingMessage';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    getUser().then(({ name, email, image, description }) => {
+      this.setState(
+        { userName: name, email, image, description, isLoading: false },
+      );
+    });
+  }
+
   render() {
+    const { isLoading, userName, email, image, description } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
-        Profile
+        {isLoading
+          ? <LoadingMessage />
+          : (
+            <div>
+              <img data-testid="profile-image" src={ image } alt="Imagem do usuário" />
+              <h2>{ userName }</h2>
+              <p>{ email }</p>
+              <p>{ description }</p>
+              <Link to="/profile/edit">Editar perfil</Link>
+            </div>
+          )}
       </div>
     );
   }
