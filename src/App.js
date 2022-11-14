26b81086fd629436839ba560b1e51cdae0329a55
leticia-238 <leticiaoliveiraandrade.9@gmail.com>
2022-11-14 11:02:07 -0300

    refactor: adiciona caminho nas rotas

--- src/App.js
+++ src/App.js
@@ -9,20 +9,22 @@ import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
 import './index.css';
 
+const basePath = "/trybetunes"
+
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Login exact path="/" />
-          <Route exact path="/search" render={ () => <Search /> } />
+          <Login exact path={ basePath } />
+          <Route exact path={`${ basePath }/search`} render={ () => <Search /> } />
           <Route
-            path="/album/:id"
+            path={`${ basePath }/album/:id`}
             render={ ({ match }) => <Album { ...match.params } /> }
           />
-          <Route exact path="/favorites" render={ () => <Favorites /> } />
-          <Route exact path="/profile" render={ () => <Profile /> } />
-          <ProfileEdit exact path="/profile/edit" />
+          <Route exact path={`${ basePath }/favorites`} render={ () => <Favorites /> } />
+          <Route exact path={`${ basePath }/profile`} render={ () => <Profile /> } />
+          <ProfileEdit exact path={`${ basePath }/profile/edit`} />
           <Route path="*" render={ () => <NotFound /> } />
         </Switch>
       </BrowserRouter>
